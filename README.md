3ccad16e9b50df51e82c1ae3ab90f18b6c38cb80
Laurynas Butkus <laurynas.butkus@gmail.com>
2015-10-13 23:12:45 +0300

    Add Arduino code & update README.md

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# volvo
-Volvo RTI screen control
+## Controlling Volvo V50 '07 RTI screen with Arduino
+
+[Video of the project](https://www.youtube.com/watch?v=QqmE6McCTZU).
+
+I used Arduino 12V power adapter to power the screen.
+
+### Schemes
+
+- [Power connection](https://raw.githubusercontent.com/laurynas/volvo/master/doc/power_connection.png)
+- [Screen connection](https://raw.githubusercontent.com/laurynas/volvo/master/doc/screen_connection.png). I used `2SC945` transistor, a bit different from the one in the scheme. It worked fine.
+
+### Thanks!
+
+While digging for information I found the most useful info here:
+
+- Reinis Veps [CarPC project](http://wot.lv/category/rpi-carpc.html)
+- Niclas Lardh [discussion](http://www.svxc.se/phpBB2/viewtopic.php?t=22034)
+
---
+++ rti_screen_control.ino
@@ -0,0 +1,88 @@
+// Code based on https://github.com/festlv/screen-control/blob/master/Screen_control/
+
+const uint8_t RTI_SERIAL_TX = 4;
+enum display_mode_name {RTI_RGB, RTI_PAL, RTI_NTSC, RTI_OFF};
+const char display_modes[] = {0x40, 0x45, 0x4C, 0x46};
+
+const char brightness_levels[] = {0x20, 0x61, 0x62, 0x23, 0x64, 0x25, 0x26, 0x67, 0x68, 0x29, 0x2A, 0x2C, 0x6B, 0x6D, 0x6E, 0x2F};
+
+extern int current_display_mode;
+extern char current_brightness_level;
+extern bool send_brightness;
+
+//delay between bytes, ms
+const int rti_delay = 100;
+
+void setup() {
+  // put your setup code here, to run once:
+  rti_setup();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  rti_loop();
+}
+
+int current_display_mode = RTI_RGB;
+bool send_brightness = true;
+char current_brightness_level = 15;
+
+const int rti_bit_delay = 410;
+
+void rti_setup() {
+  pinMode(RTI_SERIAL_TX, OUTPUT);
+}
+
+inline void rti_tx_bit_delay() {
+  unsigned long wait = micros()+rti_bit_delay;
+  while (wait>micros()) {};
+}
+
+void rti_loop() {
+  rti_bitbang_tx(display_modes[current_display_mode]);
+  delay(rti_delay);
+  
+  if (send_brightness)
+    rti_bitbang_tx(brightness_levels[current_brightness_level]);
+  else
+    rti_bitbang_tx(0x40);
+  
+  delay(rti_delay);
+  
+  rti_bitbang_tx(0x83);
+  delay(rti_delay); 
+}
+
+void rti_set_brightness(char level) {
+  if (level>0 && level<16) {
+    current_brightness_level = level;
+    send_brightness = true;
+  }
+}
+
+void rti_disable_screen() {
+  current_display_mode = RTI_OFF;
+}
+
+void rti_enable_screen() {
+  current_display_mode = RTI_PAL;
+}
+
+//inverted logic serial bitbang transmit
+void rti_bitbang_tx(char byte_to_tx) {
+
+  digitalWrite(RTI_SERIAL_TX, HIGH);
+  rti_tx_bit_delay();
+
+  for (byte mask = 0x01; mask; mask <<= 1)
+    {
+      if (byte_to_tx & mask) // choose bit
+        digitalWrite(RTI_SERIAL_TX, LOW); // send 1
+      else
+        digitalWrite(RTI_SERIAL_TX, HIGH); // send 0
+    
+      rti_tx_bit_delay();
+    }
+    digitalWrite(RTI_SERIAL_TX, LOW); // restore pin to natural state
+    rti_tx_bit_delay();
+}
